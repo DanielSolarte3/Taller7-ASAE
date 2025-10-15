b899d42c4f13a39f3065f28ebf4ee30345d530ee
Daniel Solarte <120216583+DanielSolarte3@users.noreply.github.com>
2025-10-15 15:34:42 -0500

    added file to see folders

---
+++ src/main/java/co/edu/unicauca/asae/taller7/FranjasHorarias/Dominio/Modelos/Asignatura.java
@@ -0,0 +1,10 @@
+package co.edu.unicauca.asae.taller7.FranjasHorarias.Dominio.Modelos;
+
+import lombok.Data;
+
+@Data
+public class Asignatura {
+    private Integer asignaturaId;
+    private String nombre;
+    private String codigo;
+}
---
+++ src/main/java/co/edu/unicauca/asae/taller7/FranjasHorarias/Dominio/Modelos/Curso.java
@@ -0,0 +1,4 @@
+package co.edu.unicauca.asae.taller7.FranjasHorarias.Dominio.Modelos;
+
+public class Curso {
+}
