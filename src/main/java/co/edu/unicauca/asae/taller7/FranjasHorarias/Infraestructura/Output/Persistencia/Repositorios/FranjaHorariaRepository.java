9c8841447e5a4a028f4ae009827f5847aa85ca94
cmperdomo17 <cmperdomo@unicauca.edu.co>
2025-10-21 22:37:33 -0500

    feat: fix import sql and fix franjaHorariaRepository

--- src/main/java/co/edu/unicauca/asae/taller7/FranjasHorarias/Infraestructura/Output/Persistencia/Repositorios/FranjaHorariaRepository.java
+++ src/main/java/co/edu/unicauca/asae/taller7/FranjasHorarias/Infraestructura/Output/Persistencia/Repositorios/FranjaHorariaRepository.java
@@ -2,14 +2,22 @@ package co.edu.unicauca.asae.taller7.FranjasHorarias.Infraestructura.Output.Pers
 
 import co.edu.unicauca.asae.taller7.FranjasHorarias.Infraestructura.Output.Persistencia.Entidades.FranjaHorariaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface FranjaHorariaRepository extends JpaRepository<FranjaHorariaEntity, Integer> {
+
     List<FranjaHorariaEntity> findFranjaHorariaByCurso_CursoId(Integer curso_id);
 
-    //Listar franja horaria por docente. Este servicio debe mostrar las franjas horarias, espacios físicos y cursos de un docente.
-    List<FranjaHorariaEntity> findFranjaHorariaByCurso_Docente_DocenteId(Integer docente_id);
+    // Listar franja horaria por docente. Este servicio debe mostrar las franjas
+    // horarias, espacios físicos y cursos de un docente.
+    @Query("SELECT f FROM FranjaHorariaEntity f " +
+            "JOIN f.curso c " +
+            "JOIN c.docentes d " +
+            "WHERE d.personaId = :docenteId")
+    List<FranjaHorariaEntity> findFranjaHorariaByCurso_Docentes_DocenteId(@Param("docenteId") Integer docente_id);
 }
